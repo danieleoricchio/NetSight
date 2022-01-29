26df2da410fed8dce8263118f2aa2aecb41a6c63
Stefano Potenza <potenza_stefano@ismonnet.onmicrosoft.com>
2022-01-29 11:59:35 +0100

    creazione pagina di login

---
+++ NetSight/NetSight/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using XMLSerializerTcp;
+namespace NetSight
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Utente u;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAccesso_Click(object sender, RoutedEventArgs e)
+        {
+            u = new Utente(txtNomeUtente.Text, psw.Password.ToString());
+        }
+    }
+}
---
+++ NetSight/NetSight/Utente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetSight
+{
+    public class Utente
+    {
+        public string nomeUtente;
+        public string password;
+        public Utente(string nomeUtente, string password)
+        {
+            this.nomeUtente = nomeUtente;
+            this.password = password;
+        }
+    }
+}
