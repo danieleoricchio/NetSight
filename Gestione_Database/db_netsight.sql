65300055127dfa769d192e621487cbbdebc39370
MattiaDippolito <dippo.matty@gmail.com>
2022-03-26 11:30:43 +0100

    Completamento db

--- Gestione_Database/db_netsight.sql
+++ Gestione_Database/db_netsight.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Mar 26, 2022 alle 10:50
+-- Creato il: Mar 26, 2022 alle 11:28
 -- Versione del server: 10.4.6-MariaDB
 -- Versione PHP: 7.3.8
 
@@ -25,71 +25,158 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struttura della tabella `admin`
+-- Struttura della tabella `collegati`
 --
 
-CREATE TABLE `admin` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `username` varchar(40) NOT NULL,
-  `password` char(32) NOT NULL
+CREATE TABLE `collegati` (
+  `CodUtente` int(10) UNSIGNED NOT NULL,
+  `CodPC` int(10) UNSIGNED NOT NULL,
+  `orarioCollegamento` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf32;
-
---
--- Dump dei dati per la tabella `admin`
---
-
-INSERT INTO `admin` (`id`, `username`, `password`) VALUES
-(1, 'root', 'e2fc714c4727ee9395f324cd2e7f331f');
 
 -- --------------------------------------------------------
 
 --
--- Struttura della tabella `users`
+-- Struttura della tabella `edifici`
 --
 
-CREATE TABLE `users` (
-  `nome` varchar(40) NOT NULL,
-  `cognome` varchar(40) NOT NULL,
-  `dataNascita` date NOT NULL,
-  `email` varchar(40) NOT NULL,
-  `password` char(32) NOT NULL
+CREATE TABLE `edifici` (
+  `Cod` int(10) UNSIGNED NOT NULL,
+  `Nome` varchar(40) NOT NULL,
+  `Indirizzo` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf32;
 
+-- --------------------------------------------------------
+
 --
--- Dump dei dati per la tabella `users`
+-- Struttura della tabella `laboratori`
 --
 
-INSERT INTO `users` (`nome`, `cognome`, `dataNascita`, `email`, `password`) VALUES
-('Marco', 'Grigioni', '2014-10-01', 'ilgrigio01@gmail.com', 'd93591bdf7860e1e4ee2fca799911215'),
-('Giorgia', 'Nastri', '1997-11-03', 'nastri.gio@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055'),
-('Thomas', 'Cazzola', '2003-03-14', 'thomas.cazzola2003@gmail.com', '1a1dc91c907325c69271ddf0c944bc72'),
-('Piero', 'Verdi', '1989-08-01', 'verdi.piero@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055');
+CREATE TABLE `laboratori` (
+  `Cod` int(10) UNSIGNED NOT NULL,
+  `Nome` varchar(40) NOT NULL,
+  `CodEdificio` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf32;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `pc`
+--
+
+CREATE TABLE `pc` (
+  `Cod` int(10) UNSIGNED NOT NULL,
+  `Nome` varchar(40) NOT NULL,
+  `IndirizzoIP` varchar(15) NOT NULL,
+  `Stato` tinyint(1) NOT NULL DEFAULT 0 COMMENT 'Se = 0 -> Spento',
+  `CodLaboratorio` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf32;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `utenti`
+--
+
+CREATE TABLE `utenti` (
+  `Cod` int(10) UNSIGNED NOT NULL,
+  `Nome` varchar(40) NOT NULL,
+  `Cognome` varchar(40) NOT NULL,
+  `DataNascita` date NOT NULL,
+  `Email` varchar(40) NOT NULL,
+  `Password` char(32) NOT NULL,
+  `Admin` tinyint(1) NOT NULL DEFAULT 0 COMMENT 'se = 0 -> utente normale'
+) ENGINE=InnoDB DEFAULT CHARSET=utf32;
 
 --
 -- Indici per le tabelle scaricate
 --
 
 --
--- Indici per le tabelle `admin`
+-- Indici per le tabelle `collegati`
 --
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `collegati`
+  ADD PRIMARY KEY (`CodUtente`,`CodPC`),
+  ADD KEY `acceso` (`CodPC`);
 
 --
--- Indici per le tabelle `users`
+-- Indici per le tabelle `edifici`
 --
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`email`,`password`);
+ALTER TABLE `edifici`
+  ADD PRIMARY KEY (`Cod`),
+  ADD UNIQUE KEY `Nome` (`Nome`,`Indirizzo`);
+
+--
+-- Indici per le tabelle `laboratori`
+--
+ALTER TABLE `laboratori`
+  ADD PRIMARY KEY (`Cod`),
+  ADD KEY `appartiene` (`CodEdificio`);
+
+--
+-- Indici per le tabelle `pc`
+--
+ALTER TABLE `pc`
+  ADD PRIMARY KEY (`Cod`),
+  ADD KEY `siTrova` (`CodLaboratorio`);
+
+--
+-- Indici per le tabelle `utenti`
+--
+ALTER TABLE `utenti`
+  ADD PRIMARY KEY (`Cod`),
+  ADD UNIQUE KEY `Email` (`Email`) USING BTREE;
 
 --
 -- AUTO_INCREMENT per le tabelle scaricate
 --
 
 --
--- AUTO_INCREMENT per la tabella `admin`
+-- AUTO_INCREMENT per la tabella `edifici`
 --
-ALTER TABLE `admin`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+ALTER TABLE `edifici`
+  MODIFY `Cod` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `laboratori`
+--
+ALTER TABLE `laboratori`
+  MODIFY `Cod` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `pc`
+--
+ALTER TABLE `pc`
+  MODIFY `Cod` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `utenti`
+--
+ALTER TABLE `utenti`
+  MODIFY `Cod` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- Limiti per le tabelle scaricate
+--
+
+--
+-- Limiti per la tabella `collegati`
+--
+ALTER TABLE `collegati`
+  ADD CONSTRAINT `acceso` FOREIGN KEY (`CodPC`) REFERENCES `pc` (`Cod`) ON DELETE CASCADE,
+  ADD CONSTRAINT `collegato` FOREIGN KEY (`CodUtente`) REFERENCES `utenti` (`Cod`) ON DELETE CASCADE;
+
+--
+-- Limiti per la tabella `laboratori`
+--
+ALTER TABLE `laboratori`
+  ADD CONSTRAINT `appartiene` FOREIGN KEY (`CodEdificio`) REFERENCES `edifici` (`Cod`) ON DELETE CASCADE;
+
+--
+-- Limiti per la tabella `pc`
+--
+ALTER TABLE `pc`
+  ADD CONSTRAINT `siTrova` FOREIGN KEY (`CodLaboratorio`) REFERENCES `laboratori` (`Cod`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
