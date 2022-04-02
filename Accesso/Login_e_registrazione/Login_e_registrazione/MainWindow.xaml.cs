5b57e8b7beb05faa34b5d606b0257f7cf237811f
MattiaDippolito <dippo.matty@gmail.com>
2022-04-02 11:44:30 +0200

    Creazione della pagina di accesso
    Creazione della pagina di login, integrata con la parte di registrazione.
    Manca da attribuire l'evento al bottone LOGIN

---
+++ Accesso/Login_e_registrazione/Login_e_registrazione/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Login_e_registrazione
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.WindowState = WindowState.Maximized;
+        }
+
+        private void lbl_link_MouseEnter(object sender, MouseEventArgs e)
+        {
+            lbl_link.Foreground = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromArgb(200, 0, 0, 0));
+        }
+
+        private void lbl_link_MouseLeave(object sender, MouseEventArgs e)
+        {
+            lbl_link.Foreground = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromArgb(200, 255, 255, 255));
+        }
+
+        private void lbl_link_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Registrazione r = new Registrazione(this);
+            r.Show();
+            this.Hide();
+        }
+
+        private void btn_login_MouseEnter(object sender, MouseEventArgs e)
+        {
+            btn_login.Background = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromArgb(200, 255, 255, 255));
+            btn_login.Foreground = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromArgb(200, 0, 0, 0));
+        }
+
+        private void btn_login_MouseLeave(object sender, MouseEventArgs e)
+        {
+            btn_login.Background = null;
+            btn_login.Foreground = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromArgb(200, 255, 255, 255));
+        }
+    }
+}
---
+++ Accesso/Login_e_registrazione/Login_e_registrazione/Registrazione.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Navigation;
+
+namespace Login_e_registrazione
+{
+    /// <summary>
+    /// Logica di interazione per Registrazione.xaml
+    /// </summary>
+    public partial class Registrazione : Window
+    {
+        MainWindow w;
+        private static readonly HttpClient client = new HttpClient();
+        public Registrazione(MainWindow m)
+        {
+            InitializeComponent();
+            this.WindowState = WindowState.Maximized;
+            w = m;
+        }
+
+        private async void btn_registra_Click(object sender, RoutedEventArgs e)
+        {
+            string nome = txt_nome.Text;
+            string cognome = txt_cognome.Text;
+            string mail = txt_mail.Text;
+            string data = date.Text; //splitto e scrivo in forma yyyy/mm/dd
+            string password = txt_password.Text;
+            string conferma = txt_conferma.Text;
+            if (nome != "" && cognome != "" && mail != "" && data != "" && password != "" && conferma != "")
+            {
+                if (password == conferma)
+                {
+                    var values = new Dictionary<string, string>
+                    {
+                    { "nome", nome },
+                    { "cognome", cognome },
+                    { "mail", mail },
+                    { "data", data },
+                    { "password", password }
+                    };
+
+                    var content = new FormUrlEncodedContent(values);
+
+                    var response = await client.PostAsync("http://172.16.102.77/Cazzola/ConfirmRegistration.php", content);
+
+                    var responseString = await response.Content.ReadAsStringAsync();
+
+                    Console.WriteLine(responseString);
+                }
+                else
+                {
+                    MessageBox.Show("Inserire due password uguali");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Inserire tutti i dati");
+            }
+        }
+
+        private void btn_registra_MouseEnter(object sender, MouseEventArgs e)
+        {
+            btn_registra.Background = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromArgb(200, 255, 255, 255));
+            btn_registra.Foreground = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromArgb(200, 0, 0, 0));
+        }
+
+        private void btn_registra_MouseLeave(object sender, MouseEventArgs e)
+        {
+            btn_registra.Background = null;
+            btn_registra.Foreground = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromArgb(200, 255, 255, 255));
+        }
+
+        private void lbl_indietro_MouseEnter(object sender, MouseEventArgs e)
+        {
+            lbl_indietro.Foreground = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromArgb(200, 0, 0, 0));
+        }
+
+        private void lbl_indietro_MouseLeave(object sender, MouseEventArgs e)
+        {
+            lbl_indietro.Foreground = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromArgb(200, 255, 255, 255));
+        }
+
+        private void lbl_indietro_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            w.Show();
+            this.Close();
+        }
+    }
+}
