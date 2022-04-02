8291d9a92c59dfc118fc84adec16b5dd0574408a
ThomasCazzola <81413653+ThomasCazzola@users.noreply.github.com>
2022-04-02 11:50:17 +0200

    Update ConfirmRegistration.php

--- Gestione_Database/Server/Gestione_Accessi_e_Registrazioni/ConfirmRegistration.php
+++ Gestione_Database/Server/Gestione_Accessi_e_Registrazioni/ConfirmRegistration.php
@@ -1,19 +1,20 @@
 <?php
+session_start();
+require_once "config.php";
+
 //save datas from client
 $email = $_POST['mail'];
 $nome = $_POST['nome'];
 $cognome = $_POST['cognome'];
 $dataDiNascita = $_POST['data'];
+$password = $_POST['password'];
 
 //controllo che non manchi nessun campo
-if(!empty($email) && !empty($nome) && !empty($cognome) && !empty($dataDiNascita)){
-    //controlla
-    InsertSQLWithRandomPassword($link,$nome,$cognome,$email,$dataDiNascita);
-    if(mysqli_num_rows($result)==1){
-        //setto la sessione
-        $_SESSION['user_id'] = $nome;
+if(!empty($email) && !empty($nome) && !empty($cognome) && !empty($dataDiNascita) && !empty($password)){
+    //
+    //Creo l'SQL di inserimento con una password random
+    if($result = InsertSQL($link,$nome,$cognome,$email,$dataDiNascita,$password)){
         echo "Successful";
-        //SendPacket($userIP, $socket);
     }else{
         header('Location: index.html');
     }
@@ -21,7 +22,7 @@ if(!empty($email) && !empty($nome) && !empty($cognome) && !empty($dataDiNascita)
 else{
     echo "Error, cannot execute the insert to database SQL...";
 }
-
+//
 //funzione che crea una password a caso
 function RandomPassword($length){
     $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -32,12 +33,18 @@ function RandomPassword($length){
     }
     return $randomString;
 }
-
+//
 //funzione che crea l'sql di INSERT
 function InsertSQLWithRandomPassword($link, $nome, $cognome, $email, $dataDiNascita){
     //gli passo la lunghezza della password
     $password = RandomPassword(10);
-    $sql= "INSERT INTO `users`(`nome`, `cognome`, `dataNascita`, `email`, `password`) VALUES (".$nome.",".$cognome.",".$dataDiNascita.",".$email.";".md5($password).")";
-    mysqli_query($link,$sql);
+    $sql= "INSERT INTO `utenti`(`nome`, `cognome`, `dataNascita`, `email`, `password`) VALUES (".$nome.".,".$cognome.",".$dataDiNascita.",".$email.";".md5($password).")";
+    return mysqli_query($link,$sql);
+}
+
+function InsertSQL($link, $nome, $cognome, $email, $dataDiNascita, $password){
+    $sql= 'INSERT INTO `utenti`(`nome`, `cognome`, `dataNascita`, `email`, `password`) VALUES ("'.$nome.'"'.','.'"'.$cognome.'"'.','.'"'.$dataDiNascita.'"'.','.'"'.$email.'"'.','.'"'.md5($password).'")';
+    //echo $sql;
+    return mysqli_query($link,$sql);
 }
 ?>
