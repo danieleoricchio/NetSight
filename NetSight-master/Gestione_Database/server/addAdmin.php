ec645df66918c446edef4ceb006f18e16da6c29e
StePote <stefano03.stefano.sp@gmail.com>
2022-05-10 19:54:00 +0200

    Update addAdmin.php

--- NetSight-master/Gestione_Database/server/addAdmin.php
+++ NetSight-master/Gestione_Database/server/addAdmin.php
@@ -1,44 +1,25 @@
 <?php
 require_once "config.php";
-/* if ($_SERVER["REQUEST_METHOD"] == "POST") { */
-/* $email = $_POST["email"];
-$codLab= $_POST["laboratorio"]; */
-$email = "nuovo@gmail.com";
-$nome = "nuovo";
-$cognome = "nuovoCogn";
-$dataNascita = "2001-02-02";
-$password = "ciao";
-$codlab = "1";
-$query = "SELECT * FROM admin WHERE email='$email'";
+$email = $_GET['email'];
+$codlab = $_GET['codLab'];
+$query = "SELECT * FROM admin WHERE admin.email='$email'";
 if ($result = mysqli_query($link, $query)) {
-    if (mysqli_num_rows($result) == 1) {
-        $query = " SELECT laboratori.nome,admin.Cod FROM admin,gestione_laboratori,laboratori WHERE admin.email='admin@gmail.com'AND admin.cod=gestione_laboratori.codadmin AND gestione_laboratori.codlab=laboratori.cod";
-        if ($result = mysqli_query($link, $query)) {
-            echo "ciao";
-            if (mysqli_num_rows($result) > 0) {
-                echo "admin già esistente. Laboratori associati ad esso:<br>";
-                while ($row = mysqli_fetch_array($result)) {
-                    echo $row['nome'] . "<br>";
-                }
-            }
-        }
-    } else {
-        $query = "INSERT INTO admin (nome,cognome,dataNascita, email, password) VALUES ('$nome','$cognome','$dataNascita','$email',MD5('$password'))";
-        if ($result = mysqli_query($link, $query)) {
-            $query = "SELECT * FROM admin WHERE admin.email='$email'";
-            if ($result = mysqli_query($link, $query)) {
-                if (mysqli_num_rows($result) > 0) {
-                    echo "ciao";
-                    $row = mysqli_fetch_array($result);
-                    $codAdmin = $row['Cod'];
-                    $query = "INSERT INTO gestione_laboratori (codLab, codAdmin) VALUES ('$codlab','$codAdmin')";
-                    $result = mysqli_query($link, $query);
-                    if ($result) {
-                        echo ("operazione riuscita");
-                    }
-                }
-            }
+    if (mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_array($result);
+        $codAdmin = $row['Cod'];
+        $query = "INSERT INTO gestione_laboratori (codLab, codAdmin) VALUES ('$codlab','$codAdmin')";
+        $result = mysqli_query($link, $query);
+        if ($result) {
+            $data = array("status" => "ok", "message" => "operazione eseguita con successo");
+            header("Content-Type: application/json");
+            echo json_encode($data);
+            exit();
+        }else{
+            $data = array("status" => "error", "message" => "admin non trovato");
+            header("Content-Type: application/json");
+            echo json_encode($data);
+            exit();
         }
     }
 }
-//}
+?>
